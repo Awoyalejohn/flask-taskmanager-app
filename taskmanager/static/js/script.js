fd6e6a0f684d9379e77cb55173e475db7bea3515
John Awoyale <awoyalejohn@gmail.com>
2022-05-10 19:50:31 +0100

    adding tasks 7:50

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
   // sidenav initialisation
-    let sidenav = document.querySelectorAll('.sidenav');
+  let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+    
+  // date picker initialisation
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmm, yyyy",
+    i18n: {okay: "select"}
+  });
   });
